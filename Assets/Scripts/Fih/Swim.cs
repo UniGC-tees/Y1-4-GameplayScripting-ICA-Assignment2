97cf687ecd1f4e6523b414048f4cce7e8fefa8b4
Uni <168829917+UniGC@users.noreply.github.com>
2026-03-03 16:33:36 +0000

    fin line up to water (woah very cool)

--- Assets/Scripts/Fih/Swim.cs
+++ Assets/Scripts/Fih/Swim.cs
@@ -5,6 +5,7 @@ public class Swim : MonoBehaviour
 {
     private float swimAngle;
     [SerializeField] private float swimSpeed;
+    public LayerMask mask;
 
     private void Start()
     {
@@ -18,10 +19,36 @@ public class Swim : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + swimAngle, 0);
 
         transform.position += -transform.forward * swimSpeed;
+
+        AlignToWater();
     }
 
     private void Nudge()
     {
         swimAngle =+ Random.Range(-2,2);
+    }
+
+    // just trying out overloads (woah)
+    private void Nudge(int angleRange)
+    {
+        swimAngle = +Random.Range(-angleRange, angleRange);
+    }
+
+    private void AlignToWater()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + new Vector3(0,10000,0), -transform.up, out hit, 30000f, mask, QueryTriggerInteraction.Collide))
+        {
+            transform.position = hit.point - new Vector3(0,0,0);
+
+            Debug.Log("hit");
+
+
+            // Debug.DrawRay(transform.position + new Vector3(0, 50, 0), -transform.up, new Color(50, 50, 50), 30f);
+        }
+        else
+        {
+            Debug.Log("no hit");
+        }
     }
 }
